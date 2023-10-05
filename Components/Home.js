f701d999f7045d7f818874205591a4a781378f8c
tanvir <tanvirulislam149@gmail.com>
2023-10-05 18:36:52 +0600

    data fetch done

--- Components/Home.js
+++ Components/Home.js
@@ -1,17 +1,55 @@
-import { View, Text, Image, StyleSheet, ImageBackground } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, ImageBackground, ScrollView, Alert, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 const logo = require("../assets/app-icon.png");
 const bg = require("../assets/bg.jpg");
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+      .then((res) => {
+        setData(res.data.meals);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        Alert.alert(error);
+      })
+  }, [])
+
+
   return (
     <ImageBackground style={styles.bgImage} source={bg}>
       <View style={styles.container}>
-        <View style={styles.innerContainer}>
-          <Image style={styles.logo} source={logo} />
-          <Text style={styles.logoText}>FOODIE Restaurant</Text>
-          <Text style={styles.logoSubText}>Good food & great vides!</Text>
-        </View>
+        <ScrollView>
+          <View style={styles.innerContainer}>
+            <Image style={styles.logo} source={logo} />
+            <Text style={styles.logoText}>FOODIE Restaurant</Text>
+            <Text style={styles.logoSubText}>Good food & great vides!</Text>
+          </View>
+          <View>
+            <Text style={styles.menuText}>MENU</Text>
+            <View>
+              {
+                loading ?
+                  <ActivityIndicator style={{ marginTop: 50 }} color={"white"} size={40} /> :
+                  data.length ?
+                    data.map(d =>
+                      <View key={d.idMeal}>
+                        <Image style={styles.mealImg} source={{ uri: d.strMealThumb }} />
+                        <Text style={styles.mealTitle}>{d.strMeal}</Text>
+                      </View>
+                    ) : <Text>No Record Found</Text>
+              }
+
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </ImageBackground>
   )
@@ -24,9 +62,9 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     alignItems: "center",
-    height: "25%",
     justifyContent: "center",
-    backgroundColor: 'rgba(0,0,0, 0.2)'
+    backgroundColor: 'rgba(0,0,0, 0.2)',
+    paddingVertical: 40
   },
   bgImage: {
     height: "100%",
@@ -37,13 +75,28 @@ const styles = StyleSheet.create({
   },
   logoText: {
     color: "white",
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "700"
   },
   logoSubText: {
     color: "lightgray",
     fontSize: 16,
     fontWeight: "700"
+  },
+  menuText: {
+    fontSize: 24,
+    color: "white",
+    textAlign: "center",
+    fontWeight: "700",
+    marginTop: 20
+  },
+  mealTitle: {
+    color: "white",
+    fontSize: 20,
+  },
+  mealImg: {
+    width: 200,
+    height: 150
   }
 })
 
