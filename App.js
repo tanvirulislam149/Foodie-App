fbdfb0b4474c89a77229cd29e5bfa150e718fa80
tanvir <tanvirulislam149@gmail.com>
2023-10-07 12:09:59 +0600

    user tab done

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'react-native';
-import Home from './Components/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MealDetails from './Components/MealDetails';
+import HomeContainer from './Components/HomeContainer';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,11 +10,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="home">
-        <Stack.Screen name="home" component={Home} options={{
-          headerTransparent: true,
+        <Stack.Screen name="homeContainer" component={HomeContainer} options={{
+          headerShown: false
         }} />
         <Stack.Screen name="mealDetails" component={MealDetails} options={{
-          title: "Details"
+          title: "Product Details"
         }} />
       </Stack.Navigator>
       <StatusBar />
---
+++ Components/HomeContainer.js
@@ -0,0 +1,38 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './Home';
+import UserTab from './UserTab';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const Tab = createBottomTabNavigator();
+
+const HomeContainer = () => {
+  return (
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'home'
+            : 'home-outline';
+          return <Ionicons name={iconName} size={size} color={color} />;
+        } else if (route.name === 'User') {
+          iconName = focused ? 'user-circle' : 'user-circle-o';
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        }
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}>
+      <Tab.Screen name="Home" component={Home} options={{
+        headerShown: false
+      }} />
+      <Tab.Screen name="User" component={UserTab} />
+    </Tab.Navigator>
+  )
+}
+
+export default HomeContainer
