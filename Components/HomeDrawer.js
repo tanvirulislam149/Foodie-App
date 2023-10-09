79691560a9a2f81dac791e73aae214f1a48b4254
tanvir <tanvirulislam149@gmail.com>
2023-10-09 23:01:10 +0600

    drawer style done

--- Components/HomeDrawer.js
+++ Components/HomeDrawer.js
@@ -9,26 +9,28 @@ import Entypo from 'react-native-vector-icons/Entypo';
 const Drawer = createDrawerNavigator();
 
 const HomeDrawer = () => {
-  const navigation = useNavigation();
   return (
-    <Drawer.Navigator initialRouteName="Home" screenOptions={({ navigation }) => ({
-      headerLeft: () =>
-        <Pressable onPress={navigation.toggleDrawer} >
-          <View style={{ flexDirection: "row", alignItems: "center", backgroundColor: 'white', marginLeft: 30, borderRadius: 20 }}>
-            <Entypo
-              name="menu"
-              style={{ fontSize: 25, color: "black", padding: 5 }} />
-            <Text style={{ color: "black", fontSize: 18, marginLeft: 5, paddingRight: 10 }}>Menu</Text>
-          </View>
-        </Pressable>
-    })}>
+    <Drawer.Navigator initialRouteName="Home" screenOptions={{
+      drawerActiveBackgroundColor: "pink",
+      drawerContentContainerStyle: {
+        flex: 1,
+        justifyContent: "center"
+      },
+      drawerLabelStyle: {
+        fontSize: 20,
+        color: "black"
+      },
+      headerLeft: () => <MenuIcon />
+    }}>
       <Drawer.Screen name="Home" component={Home} options={{
         title: "Home",
         headerTransparent: true,
-        headerTitle: ''
+        headerTitle: '',
+        drawerIcon: () => <Entypo style={{ marginLeft: 40 }} name="home" size={25} />
       }} />
       <Drawer.Screen name="contact" component={Contact} options={{
-        title: "Contact"
+        title: "Contact",
+        drawerIcon: () => <Entypo style={{ marginLeft: 40 }} name="phone" size={25} />
       }} />
     </Drawer.Navigator>
   )
@@ -36,10 +38,16 @@ const HomeDrawer = () => {
 
 export default HomeDrawer
 
-const MenuName = () => {
+const MenuIcon = () => {
+  const navigation = useNavigation();
   return (
-    <View style={{ backgroundColor: "white" }}>
-      <Text style={{ color: "black", padding: 10 }}>something</Text>
-    </View>
+    <Pressable onPress={navigation.toggleDrawer} >
+      <View style={{ flexDirection: "row", alignItems: "center", backgroundColor: 'white', marginLeft: 30, borderRadius: 20 }}>
+        <Entypo
+          name="menu"
+          style={{ fontSize: 25, color: "black", padding: 5 }} />
+        <Text style={{ color: "black", fontSize: 18, marginLeft: 5, paddingRight: 10 }}>Menu</Text>
+      </View>
+    </Pressable>
   )
 }
